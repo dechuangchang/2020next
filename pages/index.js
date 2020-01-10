d6c2493d1ff126ca1852d989aa366d067f647e04
= <=>
2020-01-10 16:38:49 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
 
 import "./index.less"
 import React from 'react'
+import fetch from 'node-fetch'
 import Seo from '../components/Seo'
-
 class App extends React.Component {
   static async getInitialProps({ req }) {
-    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-    return { userAgent }
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+    const res = await fetch('https://api.github.com/repos/zeit/next.js');
+    const json = await res.json();
+    return { json }
   }
 
   render() {
+
     return (
       <React.Fragment>
         <Seo title={'首页|漫画'} keywords={''} description={''} />
         <div>
-          Hello World {this.props.userAgent}
+          Hello World {JSON.stringify(this.props.json)}
         </div>
       </React.Fragment>
     )
